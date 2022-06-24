83a64984abc707ca6f1c496543e981dd138b4af5
Adrian Gomes <adrianhgomes@gmail.com>
2022-06-24 02:16:02 -0400

    formatted output

--- compiler/src/main.ml
+++ compiler/src/main.ml
@@ -1,9 +1,19 @@
 open Generator
 
+let format s =
+  let add_nl i ch =
+    let end_ch = if (i + 1) mod 80 = 0 then "\n" else "" in
+    (String.make 1 ch) ^ end_ch
+  in
+  List.init (String.length s) (String.get s)
+  |> List.mapi add_nl
+  |> String.concat ""
+
 let cmp oc =
   Lexing.from_channel (open_in Sys.argv.(1))
   |> Parser.prog Lexer.token
   |> gen_prog
+  |> format
   |> output_string oc
   ; flush stdout; close_out oc
 
